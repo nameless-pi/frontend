83aefaa57c1c8e8d182b2d6e48fdd7a2877e0633
gabrielCorreaCordeiro <gabrielcordeiro@alunos.utfpr.edu.br>
2017-09-30 20:15:08 -0300

    modal de edição da sala, funcionando e relatando erros

--- src/app/list-sala/list-sala.component.ts
+++ src/app/list-sala/list-sala.component.ts
@@ -1,6 +1,11 @@
+import { ModalSalaComponent } from './modal-sala/modal-sala.component';
+import { ModalHorarioComponent } from './modal-horario/modal-horario.component';
+import { DialogService } from 'ng2-bootstrap-modal';
 import { Component, OnInit } from '@angular/core';
 
 import { DatabaseService } from '../servicos/database.service';
+
+
 
 @Component({
   selector: 'app-list-sala',
@@ -10,32 +15,35 @@ import { DatabaseService } from '../servicos/database.service';
 
 export class ListSalaComponent implements OnInit {
   id = -1;
-  salas = [];
+  salas: any = [];
 
-  constructor(private dbService: DatabaseService) {}
+  constructor(private dbService: DatabaseService, private dialogService: DialogService, ) {}
 
   ngOnInit() {
     this.getSalas();
   }
 
-  onclick(nome) {
-    for (let i = 0; i < this.salas[0].length; i++) {
-      if (this.salas[0][i].nome === nome) {
-        this.id = i;
-        return;
-      }
-    }
-    return -1;
+  onclick(idx) {
+      this.id = idx;
   }
 
   getSalas() {
     this.dbService.getSalas()
-      .subscribe(data => this.salas.push(data));
+      .subscribe(data => this.salas = data);
   }
 
   deletarSala(index) {
-    this.dbService.deletarSala(this.salas[0][index].nome)
+    this.dbService.deletarSala(this.salas[index].id)
       .subscribe();
-    this.salas[0].splice(index);
+    this.salas.splice(index);
+  }
+
+  editarSala(id) {
+    const disposable = this.dialogService.addDialog(ModalSalaComponent, {
+      title: 'Horário - Editar',
+      sala: this.salas[id],
+    })
+      .subscribe((isConfirmed) => {});
+
   }
 }
--- src/app/list-sala/modal-sala/modal-sala.component.ts
+++ src/app/list-sala/modal-sala/modal-sala.component.ts
@@ -1,15 +1,68 @@
+import { DatabaseService } from './../../servicos/database.service';
 import { Component, OnInit } from '@angular/core';
+import { DialogComponent, DialogService } from 'ng2-bootstrap-modal';
+import {Observable} from 'rxjs/Observable';
+
+export interface ConfirmModel {
+  title: string;
+  sala: any;
+}
 
 @Component({
   selector: 'app-modal-sala',
   templateUrl: './modal-sala.component.html',
   styleUrls: ['./modal-sala.component.css']
 })
-export class ModalSalaComponent implements OnInit {
+export class ModalSalaComponent extends DialogComponent<ConfirmModel, boolean>
+  implements ConfirmModel, OnInit {
+  title: string;
+  sala: any;
 
-  constructor() { }
+  constructor(dialogService: DialogService, private dbService: DatabaseService) {
+    super(dialogService);
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(formModalSala) {
+    if (formModalSala.touched && formModalSala.valid) {
+      this.sala.nome = formModalSala.value.nome_sala;
+      this.alterarSala(this.sala);
+    }
+  }
+
+  alterarSala(sala) {
+      this.dbService
+        .editarSala(sala.id, {'nome': this.sala.nome}).catch(this.handleError)
+        .subscribe();
+      this.close();
+  }
+
+
+
+
+  public handleError(error: any) {
+    const  errMsg = error.status;
+    if ( errMsg === 403 ) {
+
+      alert( 'Este usuário ja existe!' );
+
+    }else if ( errMsg === 400) {
+
+      alert( 'ops, há algo errado nesta página ou configurações do servidor' );
+
+    }else if ( errMsg === 401) {
+
+      alert( 'Login ou senha invalido' );
+
+    }else if ( errMsg === 404) {
+
+      alert( 'Dado não encontrado!' );
+
+    }else if ( errMsg === 0) {
+      alert('Erro de conexão, tente novamente!');
+    }
+  return Observable.throw(errMsg);
+  }
 }
